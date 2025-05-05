ddc5c649cd44a168478bb83faeceb59fb1c0c44c
gitdamilare <damilareoyebanji@gmail.com>
2025-05-05 13:55:39 +0200

    Add IBankStatementParser registration and update usings
    - Registered `IBankStatementParser` with `PdfBankStatementParser` in `DependencyInjection.cs`.
    - Removed unused using directives and adjusted namespace declaration for consistency.
    - Added global using directives for `Identity`, `EntityFrameworkCore`, and other infrastructure components in `GlobalUsings.cs` to improve accessibility.

--- src/Infrastructure/GlobalUsings.cs
+++ src/Infrastructure/GlobalUsings.cs
@@ -4,8 +4,18 @@ global using Microsoft.Extensions.Logging;
 global using System.Globalization;
 global using System.Text.RegularExpressions;
 global using UglyToad.PdfPig;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Hosting;
 
 global using TaxReturnAutomation.Domain.Enums;
 global using TaxReturnAutomation.Application.Common.Interfaces;
 global using TaxReturnAutomation.Domain.Entities;
+global using TaxReturnAutomation.Infrastructure.Data;
+global using TaxReturnAutomation.Infrastructure.Data.Interceptors;
+global using TaxReturnAutomation.Infrastructure.Identity;
+global using TaxReturnAutomation.Infrastructure.Storage;
+global using TaxReturnAutomation.Infrastructure.Parsing;
 
