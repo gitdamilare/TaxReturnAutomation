ea3997ca36250fac7d4d477390fc1b0e1157e782
gitdamilare <damilareoyebanji@gmail.com>
2025-05-05 09:56:23 +0200

    Update integration tests and project configuration
    Created `Infrastructure.IntegrationTests.csproj` targeting .NET 9.0 with necessary package references for testing. Added two sample PDF files to the output directory and referenced the `Infrastructure.csproj` project.
    
    Introduced `PdfBankStatementParserTests` class with two test methods to validate the `PdfBankStatementParser` functionality, ensuring correct transaction extraction from sample PDFs and logging results for inspection.
    
    Update package versions and add new dependencies
    
    - Upgraded `FluentAssertions` from `6.12.2` to `8.2.0`.
    - Added `Azure.Storage.Blobs` (version `12.24.0`).
    - Introduced new packages: `NSubstitute` (5.3.0), `PdfPig` (0.1.10), and `Tabula` (0.1.5).
    
    Add Infrastructure.IntegrationTests project to solution
    
    - Introduced a new project "Infrastructure.IntegrationTests" with its project file located at "tests\Infrastructure.IntegrationTests\Infrastructure.IntegrationTests.csproj".
    - Updated solution configuration to include Debug and Release build configurations for the new project.
    - Added the new project to the nested projects section, linking it with an existing project GUID.

---
+++ tests/Infrastructure.IntegrationTests/PdfBankStatementParserTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using NUnit.Framework;
+using TaxReturnAutomation.Application.Common.Interfaces;
+using TaxReturnAutomation.Infrastructure.Parsing;
+
+namespace Infrastructure.IntegrationTests;
+[TestFixture]
+public class PdfBankStatementParserTests
+{
+    private readonly string _sampleFilePath = Path.Combine("Assets", "BankStatementSample_1.pdf");
+    private readonly string _sampleFilePath2 = Path.Combine("Assets", "BankStatementSample_2.pdf");
+
+    [Test]
+    public void ExtractTransactions_ShouldParseBankTransactions_FromPdf()
+    {
+        // Arrange
+        var parser = new PdfBankStatementParser(
+            Substitute.For<IFileStorageService>(),
+            NullLogger<PdfBankStatementParser>.Instance);
+
+        // Act
+        var result = parser.ExtractTransactions(_sampleFilePath);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Transactions.Should().NotBeNullOrEmpty();
+        result.Transactions.Should().HaveCount(5);
+        result.Transactions.Should().AllSatisfy(transaction =>
+        {
+            transaction.TransactionDate.Should().NotBe(default);
+            transaction.Description.Should().NotBeNullOrWhiteSpace();
+            transaction.TransactionType.Should().BeDefined();
+            transaction.Amount.Should().BePositive();
+        });
+
+        foreach (var transaction in result.Transactions)
+        {
+            TestContext.WriteLine($"" +
+                $"{transaction.TransactionDate:d} | " +
+                $"{transaction.Description} | " +
+                $"{transaction.TransactionType.ToString()} | " +
+                $"{transaction.Amount}");
+        }
+    }
+
+    [Test]
+    public void ExtractTransactions_ShouldParseBankTransactions_FromPdf2()
+    {
+        // Arrange
+        var parser = new PdfBankStatementParser(
+            Substitute.For<IFileStorageService>(),
+            NullLogger<PdfBankStatementParser>.Instance);
+
+        // Act
+        var result = parser.ExtractTransactions(_sampleFilePath2);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Transactions.Should().NotBeNullOrEmpty();
+        result.Transactions.Should().HaveCount(10);
+        result.Transactions.Should().AllSatisfy(transaction =>
+        {
+            transaction.TransactionDate.Should().NotBe(default);
+            transaction.Description.Should().NotBeNullOrWhiteSpace();
+            transaction.TransactionType.Should().BeDefined();
+            transaction.Amount.Should().BePositive();
+        });
+
+        foreach (var transaction in result.Transactions)
+        {
+            TestContext.WriteLine($"" +
+                $"{transaction.TransactionDate:d} | " +
+                $"{transaction.Description} | " +
+                $"{transaction.TransactionType.ToString()} | " +
+                $"{transaction.Amount}");
+        }
+    }
+}
